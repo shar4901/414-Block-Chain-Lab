8b3292ac15f94a102679ce557d514f3aba1ad773
shar4901 <90164014+shar4901@users.noreply.github.com>
2022-02-01 19:49:23 -0700

    Add project files.

---
+++ 414 Block Chain Lab/Blockchain.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+
+namespace _414_Block_Chain_Lab
+{
+    class Blockchain
+    {
+        public List<Block> Chain { get; set; }
+
+        public int Difficulty { get; set; }
+        public List<Transaction> pendingTransactions { get; set; }
+        public decimal MiningReward { get; set; }
+
+
+        public Blockchain(int difficulty, decimal miningReward)
+        {
+            this.Chain = new List<Block>();
+            this.Chain.Add(CreateGenesisBlock());
+            this.Difficulty = difficulty;
+            this.MiningReward = miningReward;
+            this.pendingTransactions = new List<Transaction>();
+        }
+        public Block CreateGenesisBlock()
+        {
+            return new Block(0, DateTime.Now.ToString("yyyyMMddHHmmssffff"), new List<Transaction>());
+        }
+
+        public Block GetLastestBlock()
+        {
+            return this.Chain.Last();
+        }
+
+        public void AddBlock(Block newBlock)
+        {
+            newBlock.PreviousHash = this.GetLastestBlock().Hash;
+            newBlock.Hash = newBlock.CalculateHash();
+            this.Chain.Add(newBlock);
+        }
+
+
+        public void addPendingTransaction(Transaction transaction)
+        {
+            if (transaction.FromAddress is null || transaction.ToAddress is null)
+            {
+                throw new Exception("Transactions must include a to and from address.");
+            }
+
+            if (transaction.Amount> this.GetBalanceOfWallet(transaction.FromAddress))
+            {
+                throw new Exception("There must be sufficient money in the wallet!");
+            }
+
+            if (transaction.IsValid()==false)
+            {
+                throw new Exception("Cannot add invalid transaction to a block.");
+
+            }
+
+            this.pendingTransactions.Add(transaction);
+        }
+
+        public decimal GetBalanceOfWallet (PublicKey address)
+
+        {
+            string addressDER = BitConverter.ToString(address.toDer()).Replace("-", "");
+            
+            decimal balance = 0;
+            foreach(Block block in this.Chain)
+            {
+                foreach(Transaction transaction in block.Transactions)
+                {
+                    if(!(transaction.FromAddress is null))
+                    {
+                        string fromDER = BitConverter.ToString(transaction.FromAddress.toDer()).Replace("-", "");
+                        if (fromDER == addressDER)
+                        {
+                            balance -= transaction.Amount;
+                        }
+                    }
+                    string toDER = BitConverter.ToString(transaction.ToAddress.toDer()).Replace("-", "");
+                    if (toDER==addressDER)
+                    {
+                        balance += transaction.Amount;
+                    }
+                }
+            }
+            return balance;
+        }
+        
+
+        public void MinePendingTransactions(PublicKey miningRewardWallet)
+        {
+            Transaction rewardTx = new Transaction(null, miningRewardWallet, MiningReward);
+            this.pendingTransactions.Add(rewardTx);
+
+            Block newBlock = new Block(GetLastestBlock().Index + 1, DateTime.Now.ToString("yyyyMMddHHmmssffff"), this.pendingTransactions, GetLastestBlock().Hash);
+            newBlock.Mine(this.Difficulty);
+
+            Console.WriteLine("Block successfully mined!");
+            this.Chain.Add(newBlock);
+            this.pendingTransactions = new List<Transaction>();
+        }
+
+        public bool IsChainValid()
+        {
+            for (int i = 1; i < this.Chain.Count; i++)
+            {
+                Block currentBlock = this.Chain[i];
+                Block previousBlock = this.Chain[i - 1];
+
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+
+}
---
+++ 414 Block Chain Lab/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace _414_Block_Chain_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrivateKey key1 = new PrivateKey();
+            PublicKey wallet1 = key1.publicKey();
+
+            PrivateKey key2 = new PrivateKey();
+            PublicKey wallet2 = key2.publicKey();
+
+            Blockchain rootcoin = new Blockchain(6,100);
+
+            Console.WriteLine("Start the Miner.");
+            rootcoin.MinePendingTransactions(wallet1);
+            Console.WriteLine("\nBalance of wallet1 is $" + rootcoin.GetBalanceOfWallet(wallet1).ToString());
+
+            Transaction tx1 = new Transaction(wallet1, wallet2, 10);
+            tx1.SignTransaction(key1);
+            rootcoin.addPendingTransaction(tx1);
+            Console.WriteLine("Start the Miner.");
+            rootcoin.MinePendingTransactions(wallet2);
+            Console.WriteLine("\nBalance of wallet1 is $" + rootcoin.GetBalanceOfWallet(wallet1).ToString());
+            Console.WriteLine("\nBalance of wallet2 is $" + rootcoin.GetBalanceOfWallet(wallet2).ToString());
+
+
+            string blockJSON = JsonConvert.SerializeObject(rootcoin, Formatting.Indented);
+            Console.WriteLine(blockJSON);
+
+            if (rootcoin.IsChainValid())
+            {
+                Console.WriteLine("Blockchain is valid!!");
+            }
+            else
+            {
+                Console.WriteLine("Blockchain is sad");
+            }
+        }
+    }
+}
